d97d841d5ec3361f21bcf426f61b3a9c3157b4ec
ymj68520 <x68520@gmail.com>
2022-11-29 18:14:22 +0800

    modified:   "LeetCode/1758_\347\224\237\346\210\220\344\272\244\346\233\277\344\272\214\350\277\233\345\210\266\345\255\227\347\254\246\344\270\262\347\232\204\346\234\200\345\260\221\346\223\215\344\275\234\346\225\260.cc"

--- LeetCode/1758_生成交替二进制字符串的最少操作数.cc
+++ LeetCode/1758_生成交替二进制字符串的最少操作数.cc
@@ -11,10 +11,17 @@ public:
     int minOperations(string s) {
         int n = s.length();
         if(n == 1)  return 0;
-        int cnt[2]; memset(cnt,0,sizeof(cnt));
-        for(int i=0;i<n;i++){
-            
-        }
-        return min(cnt[0],cnt[1]);
+        int cnt=0;
+        for (int i = 0; i < n; i++) cnt += (s[i] - '0') ^ (i & 1);
+        return min(cnt,n-cnt);
     }
 };
+
+int main(){
+    string s = "01001111111";
+    Solution sol;
+
+    cout << sol.minOperations(s) << endl;
+
+    return 0;
+}
