c36f4b7aae6c0250007dc4d14d3628875bd62e45
ymj68520 <x68520@gmail.com>
2022-12-12 16:28:45 +0800

    new file:   "LeetCode/1781_\346\211\200\346\234\211\345\255\227\347\254\246\344\270\262\347\276\216\344\270\275\345\200\274\344\271\213\345\222\214.cc"

---
+++ LeetCode/1781_所有字符串美丽值之和.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans = 0;
+        int n = s.size();
+        int cnt[26];
+        for (int i = 0; i < n; ++i) {
+            memset(cnt, 0, sizeof cnt);
+            for (int j = i; j < n; ++j) {
+                ++cnt[s[j] - 'a'];
+                int mi = 1000, mx = 0;
+                for (int& v : cnt) {
+                    if (v > 0) {
+                        mi = min(mi, v);
+                        mx = max(mx, v);
+                    }
+                }
+                ans += mx - mi;
+            }
+        }
+        return ans;
+    }
+};
